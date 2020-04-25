dea7dcd14ed49a9fc543a8f0a442fa1537bb50d7
Tymoteusz <tymoteuszsloninka@interia.pl>
2020-04-25 13:48:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Twój email to: {{ email }}</h1>
+	<div v-if="email.length < 10">Ale masz krótki adres!</div>
+	<div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+	<div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+	<input type='text' v-model="email">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+	return {
+		email: ''
+	};
+  }
+}
+</script>
+
+<style>
+
+</style>
